a086445c4eabcfa65d42d84f8c351cb493985cfc
Christophe Duchesne <cduchesne.1@gmail.com>
2023-01-01 18:58:33 -0500

    chore: setup material ui theme

---
+++ theme/palette.ts
@@ -0,0 +1,28 @@
+const palette = {
+  primary: {
+    main: '#23262f',
+    contrastText: '#fcfcfd',
+  },
+  secondary: {
+    main: '#777e91',
+    contrastText: '#fcfcfd',
+  },
+  error: {
+    main: '#fa8f54',
+    contrastText: '#fcfcfd',
+  },
+  warning: {
+    main: '#ffd166',
+    contrastText: '#fcfcfd',
+  },
+  info: {
+    main: '#3b71fe',
+    contrastText: '#fcfcfd',
+  },
+  success: {
+    main: '#58c27d',
+    contrastText: '#fcfcfd',
+  },
+};
+
+export default palette;
